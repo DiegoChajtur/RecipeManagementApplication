a3176acf0cd232a2f06ab710ffc30fd2d1b2004e
DiegoChajtur <diego.chajtur@gmail.com>
2025-04-21 00:46:25 -0400

    chg: cors

--- recipes/src/main/java/com/pratice/recipes/Recipe/RecipeController.java
+++ recipes/src/main/java/com/pratice/recipes/Recipe/RecipeController.java
@@ -15,6 +15,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/recipes")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:4200")
 public class RecipeController {
 
     private final RecipeService recipeService;
